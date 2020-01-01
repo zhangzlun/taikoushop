49b7ebfa5a90865f5771e83a2763e577872e0577
張志綸 <k0926521654@gmail.com>
2020-01-01 18:52:09 +0900

    20200101

---
+++ taikoushop-app-web/src/components/NavBar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TextField from "@material-ui/core/TextField";
+
+
+const NavBar = () => {
+
+
+    return (
+        <div>
+            <TextField id="standard-basic" label="Standard" />
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ taikoushop-app-web/src/views/SelectGoods/index.js
@@ -0,0 +1,10 @@
+import React, {useEffect}  from 'react';
+
+function SelectGoods() {
+
+    useEffect(() => {
+
+    });
+
+    return <div>3456789</div>
+}
